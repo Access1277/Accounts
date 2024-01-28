ab488e89d2113e0d4ce54af54972ff914c19745d
Access1277 <108107724+Access1277@users.noreply.github.com>
2024-01-28 08:00:39 +0800

    Update boost.sh

--- boost.sh
+++ boost.sh
@@ -1,25 +1,65 @@
 #!/bin/bash
 
-CUSTOM_DIG="/data/data/com.termux/files/home/go/bin/fastdig"
+## Your DNSTT Nameserver & your Domain `A` Record
+NS='ns.dnstt.lantindns.tech'
+A='dnstt.lantindns.tech'
 
-# Function to query NS records
-query_ns_records() {
-    local domain="$1"
-    echo "Querying NS records for $domain using $CUSTOM_DIG..."
-    "$CUSTOM_DIG" +short NS "$domain"
+## Repeat dig cmd loop time (seconds) (positive interger only)
+LOOP_DELAY=1
+
+## Add your DNS here
+declare -a HOSTS=('124.6.181.12')
+
+## Linux' dig command executable filepath
+## Select value: "CUSTOM|C" or "DEFAULT|D"
+DIG_EXEC="DEFAULT"
+## if set to CUSTOM, enter your custom dig executable path here
+CUSTOM_DIG=/data/data/com.termux/files/home/go/bin/fastdig
+
+VER=0.1
+case "${DIG_EXEC}" in
+ DEFAULT|D)
+ _DIG="$(command -v dig)"
+ ;;
+ CUSTOM|C)
+ _DIG="${CUSTOM_DIG}"
+ ;;
+esac
+if [ ! $(command -v ${_DIG}) ]; then
+ printf "%b" "Dig command failed to run, " \
+ "please install dig(dnsutils) or check " \
+ "\$DIG_EXEC & \$CUSTOM_DIG variable inside $( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/$(basename "$0") file.\n" && exit 1
+fi
+endscript() {
+ unset NS A LOOP_DELAY HOSTS _DIG DIG_EXEC CUSTOM_DIG T R M
+ exit 1
 }
-
-# Function for DNS booster with keepalive
-dns_booster() {
-    echo "Starting DNS booster with keepalive..."
-    # Add your DNS booster commands or logic here
+trap endscript 2 15
+check(){
+ for ((i=0; i<"${#HOSTS[*]}"; i++)); do
+  for R in "${A}" "${NS}"; do
+   T="${HOSTS[$i]}"
+   [[ -z $(timeout -k 10 10 ${_DIG} @${T} ${R}) ]] && M=31 || M=32;
+   echo -e "\e[1;${M}m\$ R:${R} D:${T}\e[0m"
+   unset T R M
+  done
+ done
 }
-
-# Example usage with the provided NS value
-NS="sdns.myudph.elcavlaw.com"
-
-# Query NS records for a specific domain using the provided NS value
-query_ns_records "$NS"
-
-# Uncomment the line below if you want to enable the DNS booster
-# dns_booster
+echo "DNSTT Keep-Alive script <Lantin Nohanih>"
+echo -e "DNS List: [\e[1;34m${HOSTS[*]}\e[0m]"
+echo "CTRL + C to close script"
+[[ "${LOOP_DELAY}" -eq 1 ]] && let "LOOP_DELAY++";
+case "${@}" in
+ loop|l)
+ echo "Script loop: ${LOOP_DELAY} seconds"
+ while true; do
+  check
+  echo '.--. .-.. . .- ... .     .-- .- .. -'
+  sleep ${LOOP_DELAY}
+ done
+ ;;
+ *)
+ check
+ ;;
+esac
+exit 0
