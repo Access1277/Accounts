71ace94cbfc9bda7b2b1d49932780438b43d4124
Access1277 <108107724+Access1277@users.noreply.github.com>
2024-01-29 21:31:06 +0800

    Update boost.sh

--- boost.sh
+++ boost.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+## Copyright ©UDPTeam
+## Script to keep-alive your DNSTT server domain record query from target resolver/local dns server
+## Run this script excluded to your VPN tunnel (split vpn tunneling mode)
+## run command: ./globe-civ3.sh l
 
 ## Your DNSTT Nameserver & your Domain `A` Record
 NS='sdns.myudph.elcavlaw.com'
 A='myudph.elcavlaw.com'
-
 ## Repeat dig cmd loop time (seconds) (positive interger only)
 LOOP_DELAY=5
 
 ## Add your DNS here
-declare -a HOSTS=(''124.6.181.12" "124.6.181.4" "124.6.181.36")
+declare -a HOSTS=('112.198.126.124' '112.198.126.116' '112.198.126.44' '124.6.181.20' '124.6.181.12' '124.6.181.4' '124.6.181.36')
 
 ## Linux' dig command executable filepath
 ## Select value: "CUSTOM|C" or "DEFAULT|D"
@@ -16,6 +19,11 @@ DIG_EXEC="DEFAULT"
 ## if set to CUSTOM, enter your custom dig executable path here
 CUSTOM_DIG=/data/data/com.termux/files/home/go/bin/fastdig
 
+######################################
+######################################
+######################################
+######################################
+######################################
 VER=0.1
 case "${DIG_EXEC}" in
  DEFAULT|D)
@@ -39,13 +47,13 @@ check(){
  for ((i=0; i<"${#HOSTS[*]}"; i++)); do
   for R in "${A}" "${NS}"; do
    T="${HOSTS[$i]}"
-   [[ -z $(timeout -k 5 5 ${_DIG} @${T} ${R}) ]] && M=31 || M=32;
+   [[ -z $(timeout -k 3 3 ${_DIG} @${T} ${R}) ]] && M=31 || M=32;
    echo -e "\e[1;${M}m\$ R:${R} D:${T}\e[0m"
    unset T R M
   done
  done
 }
-echo "DNSTT Keep-Alive script <Lantin Nohanih>"
+echo "DNSTT Keep-Alive script <Dexter Eskalarte>"
 echo -e "DNS List: [\e[1;34m${HOSTS[*]}\e[0m]"
 echo "CTRL + C to close script"
 [[ "${LOOP_DELAY}" -eq 1 ]] && let "LOOP_DELAY++";
